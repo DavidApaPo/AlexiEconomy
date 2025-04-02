11d2e6810e7ecae4f54d9861c3d362fe1f47e365
DavidApaPo <davidap601@gmail.com>
2025-04-01 20:52:43 -0600

    version

---
+++ Login.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AlexiEconomy
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string patron = @"^[a-zA-Z\s]+$";
+            string texto = txtUsuario.Text;
+
+            if (!Regex.IsMatch(texto, patron) && texto != "")
+            {
+
+                MessageBox.Show("Solo se permiten letras y espacios.");
+
+                txtUsuario.Text = texto.Remove(texto.Length - 1);
+
+                txtUsuario.SelectionStart = txtUsuario.Text.Length;
+            }
+        }
+    }
+}
